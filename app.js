32b609eeeb8f51a3788ba2577e3240104344cc7f
Geet Kumar Sahu <geetsahu1852005@gmail.com>
2025-03-19 21:51:10 +0530

    bcryptjs added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
